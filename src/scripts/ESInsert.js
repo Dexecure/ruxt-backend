f3b07ca6f4b4c745b2acbc8667d079ed904d817f
Inian Parameshwaran <inian1234@gmail.com>
2019-12-02 16:35:00 +0800

    add missing _type to ES script

--- src/scripts/ESInsert.js
+++ src/scripts/ESInsert.js
@@ -50,7 +50,10 @@ function prepareResult(rows) {
   rows.forEach(row => {
     // let origin = row["origin"];
     let origin = row;
-    items.push({ index: { _index: "origin-index", _id: origin } }, { origin });
+    items.push(
+      { index: { _index: "origin-index", _type: "all", _id: origin } },
+      { origin }
+    );
   });
   return items;
 }
